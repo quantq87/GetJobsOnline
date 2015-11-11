18cf8867ed4a46f978e83cf8cf458e10bf254fa4
quantq87 <quantq777@gmail.com>
2015-11-11 23:25:15 +0700

    quan
    change

---
+++ GetJobOnline/Jobs/Constances/ConstanceInstance.h
@@ -0,0 +1,15 @@
+//
+//  ConstanceInstance.h
+//  GetJobOnline
+//
+//  Created by Quan Tran on 11/11/15.
+//  Copyright © 2015 Quan Tran. All rights reserved.
+//
+
+#ifndef ConstanceInstance_h
+#define ConstanceInstance_h
+
+#define gjo_height_nav_bar              44
+
+
+#endif /* ConstanceInstance_h */
